2c0998a22121852f43c47333a519480efbd25e8f
mifegui <ericmfguimaraes@tuta.io>
2021-02-27 18:01:28 -0300

    Melhorar exibição da tela de cadastro de cliente

--- view/cadastro_cliente_view.php
+++ view/cadastro_cliente_view.php
@@ -1,13 +1,11 @@
-<form action="index.php" method="post" id="formLogin">
-	<input type="text" name="rua" placeholder="rua">
-	<br>
-	<input type="text" name="bairro" placeholder="bairro">
-	<br>			
-	<input type="number" name="numero" placeholder="numero">
-	<br>
-	<input type="text" name="cidade" placeholder="cidade">
-	<br>
-	<input type="hidden" name="class" value="Pedido" />
-	<input type="hidden" name="acao" value="realizar_pedido" />
-	<button>Confirmar compra</button>
+<form class="form-login" action="index.php" method="post" id="formLogin">
+    <div class="form-group">
+        <input class="form-control" type="text" name="rua" placeholder="Rua">
+        <input class="form-control" type="text" name="bairro" placeholder="Bairro">
+        <input class="form-control" type="number" name="numero" placeholder="Número">
+        <input class="form-control" type="text" name="cidade" placeholder="Cidade">
+        <input type="hidden" name="class" value="Pedido" />
+        <input type="hidden" name="acao" value="realizar_pedido" />
+		<button class="btn btn-success">Confirmar Compra</button>
+    </div>
 </form>
