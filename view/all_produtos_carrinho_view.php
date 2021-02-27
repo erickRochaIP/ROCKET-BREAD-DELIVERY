923189ff6202451f5202aac9b33a3626405265b1
Erick Rocha <erickfr1011@gmail.com>
2021-02-27 16:41:02 -0300

    Exibir imagem do produto da tela principal

--- view/all_produtos_carrinho_view.php
+++ view/all_produtos_carrinho_view.php
@@ -15,7 +15,7 @@
     <?php endif; ?>
 
     <div class="card" style="background-color: #FFF6F1">
-      <!-- <img class="card-img-top" src="..." alt="Imagem do produto"> -->
+      <img class="card-img-top" src="<?php echo 'images/'.$produto->getId().'.jpg'; ?>" alt="Imagem do produto">
       <div class="card-body">
         <?php
         echo "<h4 class='card-title'>".$produto->getNome()."</h4>";
