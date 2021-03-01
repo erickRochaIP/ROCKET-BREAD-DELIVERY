543b2425473a510c8b300c1aa9f5dbb428e30b25
mifegui <ericmfguimaraes@tuta.io>
2021-03-01 19:08:09 -0300

    Melhorar exibição da tela de pedidos do cliente

--- view/pedidos_cliente_view.php
+++ view/pedidos_cliente_view.php
@@ -1,25 +1,43 @@
-<?php
-	$endereco = $_REQUEST['endereco'];
-	echo "Endereco: Numero ".$endereco->getNumero();
-	echo ", Rua ".$endereco->getRua();
-	echo ", Bairro ".$endereco->getBairro();
-	echo ", Cidade ".$endereco->getCidade();
-?>
-<br>
-<?php
-	foreach ($_REQUEST['pedidos'] as $pedido) {
-		echo 'Id Pedido: '.$pedido[0]->getId().' / Situacao: '.$pedido[1]->getSituacao();
-		?>
-		<form action="index.php" method="get">
-			<?php
-				echo '<input type="hidden" name="id" value="'.$pedido[0]->getId().'" />';
-			?>
-			<input type="hidden" name="class" value="Pedido" />
-			<input type="hidden" name="acao" value="getItensPedidos" />
-			<button>Abrir pedido</button>
-
-		</form>
-		<br>
-		<?php
-	}
-?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <table class="table">
+                <thead>
+                <tr>
+                <td>Id do pedido</td>
+                <td>Situação</td>
+                <td>Produto</td>
+                </tr>
+                <thead>
+                <tbody>
+                <?php foreach ($_REQUEST['pedidos'] as $pedido): ?>
+                <tr>
+                <td> <span><?php echo $pedido[0]->getId(); ?> </span> </td>
+                <td> <span><?php echo $pedido[1]->getSituacao(); ?> </span> </td>
+                <td>
+                    <form action="index.php" method="get">
+                        <?php
+                            echo '<input type="hidden" name="id" value="'.$pedido[0]->getId().'" />';
+                        ?>
+                        <input type="hidden" name="class" value="Pedido" />
+                        <input type="hidden" name="acao" value="getItensPedidos" />
+                        <button class="btn btn-secondary">Abrir pedido</button>
+                    </form>
+                </td>
+                </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+        <?php
+            $endereco = $_REQUEST['endereco'];
+            echo "Endreço: <br> Numero ".$endereco->getNumero();
+            echo ", Rua ".$endereco->getRua();
+            echo "<br>";
+            echo "Bairro ".$endereco->getBairro();
+            echo ", Cidade ".$endereco->getCidade();
+        ?>
+        </div>
+</div>
+</div>
