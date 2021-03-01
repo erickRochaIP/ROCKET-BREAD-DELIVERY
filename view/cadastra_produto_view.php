c88ff8195206192e91cb13df8c290bdd759301cc
mifegui <ericmfguimaraes@tuta.io>
2021-03-01 18:09:08 -0300

    Melhorar exibição da tela cadastrar novo produto

--- view/cadastra_produto_view.php
+++ view/cadastra_produto_view.php
@@ -1,20 +1,15 @@
 <div class="form-login">
+  <div class="form-group">
 	<form action="index.php" method="post" id="formLogin" enctype="multipart/form-data">
-		<input type="text" name="nome" placeholder="Nome">
-		<br>
-
-		<input type="text" name="descricao" placeholder="Descricao">
-		<br>
-
-		<input type="file" id="img" name="img" accept="image/*">
-		<br>
-
-		<input type="text" name="preco" placeholder="Preco">
-		<br>
+		<input class="form-control" type="text" name="nome" placeholder="Nome">
+		<input class="form-control" type="text" name="descricao" placeholder="Descricao">
+		<input class="form-control" type="file" id="img" name="img" accept="image/*">
+		<input class="form-control" type="text" name="preco" placeholder="Preço">
 
 		<input type="hidden" name="ativo" value="1">
 		<input type="hidden" name="class" value="Produto" />
 		<input type="hidden" name="acao" value="saveProduto" />
-		<button class="botaoLogin" value="upload">Criar Produto</button>
+		<button class="btn btn-secondary botaoLogin" value="upload">Criar Produto</button>
 	</form>
+    </div>
 </div>
