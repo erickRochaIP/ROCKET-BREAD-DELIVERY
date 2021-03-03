3572cd3ed70c3770f040adf86cfffee08366d7e3
mifegui <ericmfguimaraes@tuta.io>
2021-03-03 12:43:42 -0300

    Melhorar exibição da tela de pedidos

--- view/tela_inicial_view.php
+++ view/tela_inicial_view.php
@@ -1,47 +1,64 @@
 
-<?php
-	$usuario = $_SESSION['usuario'];
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <div class="form-group">
+            <div class="form-inline">
+            <form class="m-2" action="index.php" method="get">
+            	<input type="hidden" name="class" value="Produto">
+            	<input type="hidden" name="acao" value="cadastrarProduto">
+            	<button class="btn btn-secondary">Cadastrar Produto</button>
+            </form>
 
-	echo "Logado como: ";
-	echo "<br>";
+            <form class="m-2" action="index.php" method="get">
+            	<input type="hidden" name="class" value="Cliente">
+            	<input type="hidden" name="acao" value="getAllCliente">
+            	<button class="btn btn-secondary">Ver todos os clientes</button>
+            </form>
+            </div>
+            </div>
 
-	echo "ID: ".$usuario[0];
-	echo "<br>";
-	echo "Nome de Usuario: ".$usuario[1];
-	echo "<br>";
-	echo "Nivel acesso: ".$usuario[2];
-	echo "<br>";
-?>
+            <table class="table">
+            <thead>
+                <tr>
+                <td> Id do pedido </td>
+                <td> Situação </td>
+                <td> </td>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($_REQUEST['pedidos'] as $pedido): ?>
+                <tr>
+                <td> <?php echo $pedido[0]->getId() ?> </td>
+                <td> <?php echo $pedido[1]->getSituacaoTexto(); ?> </td>
+                <td>
+            		<form action="index.php" method="get">
+            			<?php
+            				echo '<input type="hidden" name="id" value="'.$pedido[0]->getId().'" />';
+            			?>
+            			<input type="hidden" name="class" value="Pedido" />
+            			<input type="hidden" name="acao" value="getItensPedidos" />
+            			<button class="btn">Abrir pedido</button>
 
-<form action="index.php" method="get">
-
-	<input type="hidden" name="class" value="Produto">
-	<input type="hidden" name="acao" value="cadastrarProduto">
-	<button>Cadastrar Produto</button>
-</form>
-
-<form action="index.php" method="get">
-
-	<input type="hidden" name="class" value="Cliente">
-	<input type="hidden" name="acao" value="getAllCliente">
-	<button>Ver todos os clientes</button>
-</form>
-
-<?php
-	foreach ($_REQUEST['pedidos'] as $pedido) {
-		echo 'Id Pedido: '.$pedido[0]->getId().' / Situacao: '.$pedido[1]->getSituacao();
-		?>
-		<form action="index.php" method="get">
-			<?php
-				echo '<input type="hidden" name="id" value="'.$pedido[0]->getId().'" />';
-			?>
-			<input type="hidden" name="class" value="Pedido" />
-			<input type="hidden" name="acao" value="getItensPedidos" />
-			<button>Abrir pedido</button>
-
-		</form>
-		<br>
-		<?php
-	}
-?>
-
+            		</form>
+                </td>
+                </tr>
+            		<?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-md-4">
+        <div class="mt-4 ml-2">
+        <?php
+        	$usuario = $_SESSION['usuario'];
+        
+        	echo "Logado como: ".$usuario[1];
+        	echo "<br>";
+        	echo "ID de usuário: ".$usuario[0];
+        	echo "<br>";
+        	echo "Nivel de acesso: ".$usuario[2];
+        	echo "<br>";
+        ?>
+        <div>
+        </div>
+</div>
